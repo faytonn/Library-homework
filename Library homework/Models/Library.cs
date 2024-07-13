2a9f4c8aba2219ed632164303803ffd6fb714877
Fatima <fatimavaliyeva0704@gmail.com>
2024-07-13 23:51:15 +0400

    Add project files.

---
+++ Library homework/Models/Library.cs
@@ -0,0 +1,67 @@
+﻿using _11_7_24.Classes;
+using _11_7_24.Exceptions;
+
+namespace Library_hw_for_13_07.Models
+{
+    public class Library<T>
+    {
+        private List<Book> Books = new List<Book>();
+        private static int _id;
+        public int Id { get; set; }
+        public int BookLimit
+        {
+            get
+            {
+                return BookLimit;
+            }
+            private set
+            {
+                if (BookLimit < 0)
+                {
+                    throw new Exception("Book limit should be greater than 0");
+                }
+                BookLimit = value;
+            }
+        }
+
+        public Library(int bookLimit)
+        {
+            BookLimit = bookLimit;
+            Books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            if (Books.Count > BookLimit)
+            {
+                throw new CapacityLimitException("Book count exceeded limit.");
+            }
+            Books.Add(book);
+        }
+
+        public void GetBookById(int id)
+        {
+            foreach (var book in Books)
+            {
+                if (book.Id == id)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+            throw new NotFoundException("Book with the entered ID not found.");
+        }
+        public void RemoveById(int id)
+        {
+            for (int i = 0; i < Books.Count; i++)
+            {
+                if (Books[i].Id == id)
+                {
+                    Books.Remove(Books[i]);
+                    Console.WriteLine("Book successfully removed.");
+                }
+            }
+            throw new NotFoundException("Book with the entered ID not found.");
+
+        }
+    }
+}
